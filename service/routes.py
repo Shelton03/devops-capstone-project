c03d1e7392eccc33ec94dcbed7114d9453d58098
Shelton03 <sheltonmutambirwa@gmail.com>
2026-03-10 12:30:56 +0200

    Add read account endpoint (GET /accounts/<id>)

--- service/routes.py
+++ service/routes.py
@@ -68,7 +68,18 @@ def create_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-# ... place you code here to READ an account ...
+
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def get_accounts(account_id):
+    """Retrieve a single Account
+
+    This endpoint will return an Account based on its id
+    """
+    app.logger.info("Request to retrieve Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id '{account_id}' was not found.")
+    return jsonify(account.serialize()), status.HTTP_200_OK
 
 
 ######################################################################
